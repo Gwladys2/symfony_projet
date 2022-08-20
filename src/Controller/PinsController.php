5f8ee12e20744e6f39a577d6fec425d845197a60
gwladys <gwladysakom@gmail.com>
2022-08-20 18:02:19 +0200

    panterest1

---
+++ src/Controller/PinsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PinRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinsController extends AbstractController
+{
+    /**
+     * @Route("/pins", name="app_pins")
+     */
+    public function index(PinRepository $pinRepository)
+    {
+        $pins=$pinRepository->findAll();
+        return $this->render('pins/index.html.twig', compact('pins'));
+    }
+}
